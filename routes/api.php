663800234ac705f54f29e0f0e459063bd1bd9833
Dani <d.tarazoma@geekshubs.com>
2022-05-03 11:13:42 +0200

    feat: crud routes contacts created

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/contacts', function() {
+    return 'GET ALL CONTACTS';
+});
+
+Route::get('/contact/{id}', function($id) {
+    return 'GET CONTACT BY ID-> '. $id;
+});
+
+Route::post('/contact', function() {
+    return 'CREATE CONTACT BY ID';
+});
+
+Route::put('/contacts/{id}', function($id) {
+    return 'UPDATE CONTACT BY ID'. $id;
+});
+
+Route::delete('/contacts/{id}', function($id) {
+    return 'DELETE CONTACT BY ID'. $id;;
 });
