ea681900080d50248eb10b5e3721de430710b6eb
Dani <d.tarazoma@geekshubs.com>
2022-05-04 12:48:19 +0200

    feat: refactor ContactController getAll getOne to Model

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -9,18 +10,30 @@ class ContactController extends Controller
 {
     public function getAllContacts()
     {
-        $contacts = DB::table('contacts')
-            ->select('name', 'surname as apellido')
-            ->where('id_user',"=", 1)
-            ->get()
-            ->toArray();
+        // $contacts = DB::table('contacts')
+        //     ->select('name', 'surname as apellido')
+        //     ->where('id_user',"=", 1)
+        //     ->get()
+        //     ->toArray();
 
-        return $contacts;
+        $contacts = Contact::where('id_user', 7)->get()->toArray();
+
+        if(empty($contacts)) {
+            return response()->json(["success" => "There are not contacts"], 202);
+        }
+
+        return response()->json($contacts, 200);
     }
 
     public function getContactById($id)
     {
-        return 'GET CONTACT BY ID-> '.$id;
+        $contact = Contact::where('id', $id)->where('id_user', 1)->first();
+
+        if(empty($contact)) {
+            return response()->json(["error" => "Contact not exists"], 404);
+        }
+
+        return response()->json($contact, 200);
     }
 
     public function createContact(Request $request)
