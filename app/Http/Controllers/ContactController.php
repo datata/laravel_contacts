8dec65b78f0e260aa26b9a82fa33fa2cfde41ae3
Dani <d.tarazoma@geekshubs.com>
2022-05-05 09:47:26 +0200

    feat: delete contact by id

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Contact;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class ContactController extends Controller
 {
@@ -38,6 +39,16 @@ class ContactController extends Controller
 
     public function createContact(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:100',
+            'surname' => 'required|string|max:100   ',
+            'phone_number' => 'required|string',
+            'email' => 'required|email',
+        ]);
+ 
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
 
         $newContact = new Contact();
         $newContact->name = $request->name;
@@ -53,11 +64,49 @@ class ContactController extends Controller
 
     public function updateContact(Request $request, $id)
     {
-        return 'UPDATE CONTACT BY ID-> '. $id;
+        $contact = Contact::where('id', $id)->where('id_user', 1)->first();
+
+        if(empty($contact)) {
+            return response()->json(["error" => "Contact not exists"], 404);
+        }
+
+        if(isset($request->name)){
+            $contact->name = $request->name;
+        }
+
+        if(isset($request->surname)){
+            $contact->surname = $request->surname;
+        }
+
+        if(isset($request->email)){
+            $contact->email = $request->email;
+        }
+        
+        if(isset($request->phone_number)){
+            $contact->phone_number = $request->phone_number;
+        }
+
+        $contact->save();      
+
+        // return 'Contact created';
+        // return ["data" => $contact, "success" => "Contact updated"];
+
+        return response()->json(["data" => $contact, "success" => "Contact updated"], 200);
     }
 
     public function deleteContact($id)
     {
-        return 'DELETE CONTACT BY ID-> '. $id;;
+        $contact = Contact::where('id', $id)->where('id_user', 1)->first();
+
+        if (empty($contact)) {
+            return response()->json([
+                "error" => "Este contacto no existe"
+            ],
+            404);
+        }
+
+        $contact->delete();
+
+        return response()->json([ "success"=>"Has eliminado el contacto: ".$id], 200);
     }
 }
