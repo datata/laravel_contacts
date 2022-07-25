02746014fc9529df27904f1b2bd2c7a9370d4265
Dani <d.tarazoma@geekshubs.com>
2022-07-25 17:39:10 +0200

    feat: comment endpoints

--- routes/api.php
+++ routes/api.php
@@ -25,38 +25,38 @@ Route::get('/', function () {
     return  'Agenda app';
 });
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+// Route::post('/register', [AuthController::class, 'register']);
+// Route::post('/login', [AuthController::class, 'login']);
 
-Route::group([
-    'middleware' => 'jwt.auth'
-], function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/profile', [AuthController::class, 'profile']);
-});
+// Route::group([
+//     'middleware' => 'jwt.auth'
+// ], function () {
+//     Route::post('/logout', [AuthController::class, 'logout']);
+//     Route::get('/profile', [AuthController::class, 'profile']);
+// });
 
-//CONTACTS
-Route::group([
-    'middleware' => ['jwt.auth', 'isUserActive'],
-], function () {
-    Route::get('/contacts', [ContactController::class, 'getAllContacts']);
-    Route::get('/contact/{id}', [ContactController::class, 'getContactById']);
-    Route::post('/contact', [ContactController::class, 'createContact']);
-    Route::put('/contacts/{id}', [ContactController::class, 'updateContact']);
-    Route::delete('/contacts/{id}', [ContactController::class, 'deleteContact']);
-});
+// //CONTACTS
+// Route::group([
+//     'middleware' => ['jwt.auth', 'isUserActive'],
+// ], function () {
+//     Route::get('/contacts', [ContactController::class, 'getAllContacts']);
+//     Route::get('/contact/{id}', [ContactController::class, 'getContactById']);
+//     Route::post('/contact', [ContactController::class, 'createContact']);
+//     Route::put('/contacts/{id}', [ContactController::class, 'updateContact']);
+//     Route::delete('/contacts/{id}', [ContactController::class, 'deleteContact']);
+// });
 
-// USER
-Route::group([
-    'middleware' => 'jwt.auth'
-], function () {
-    Route::get('/user-by-contact-id/{id}', [UserController::class, 'getUserByContactId']);
-});
+// // USER
+// Route::group([
+//     'middleware' => 'jwt.auth'
+// ], function () {
+//     Route::get('/user-by-contact-id/{id}', [UserController::class, 'getUserByContactId']);
+// });
 
 
-Route::group([
-    'middleware' => ['jwt.auth', 'isSuperAdmin']
-], function () {
-    Route::post('/create-user-admin/{id}', [UserController::class, 'createUserAdmin']);
-    Route::post('/destroy-user-admin/{id}', [UserController::class, 'destroyUserAdmin']);
-});
+// Route::group([
+//     'middleware' => ['jwt.auth', 'isSuperAdmin']
+// ], function () {
+//     Route::post('/create-user-admin/{id}', [UserController::class, 'createUserAdmin']);
+//     Route::post('/destroy-user-admin/{id}', [UserController::class, 'destroyUserAdmin']);
+// });
